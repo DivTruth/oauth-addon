c335c98e39221ecdd72a589377075401905618f9
Nick Worth <nworth@raprec.com>
2016-12-13 14:31:05 -0700

    restructure how oauth app handles user session

--- object-oauth-app.php
+++ object-oauth-app.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 abstract class OAuthApp {
 
-	/**
+    /**
      * Define application settings
      */
     protected $app;
@@ -20,7 +20,6 @@ abstract class OAuthApp {
     protected $client_id;
     protected $client_secret;
     protected $login_uri;
-    public $token;
 
     /**
      * Authentiation object
@@ -30,14 +29,17 @@ abstract class OAuthApp {
     public $oauth;
 
     /**
-	 * OAuthApp Constructor
-	 */
-	function __construct() {
-		$this->config_oauth();
-	}
+     * OAuthApp Constructor
+     */
+    function __construct() {
+        $this->config_oauth();
+    }
 
+    /**
+     * Initialize OAuth app
+     */
     public function init(){
-        $this->token = $this->get_user_session();
+        $this->get_user_session();
     }
 
     /**
@@ -45,28 +47,29 @@ abstract class OAuthApp {
      */
     protected function config_oauth(){
         # Create a Salesforce authenticatio object
-        if($this->oauth == ''){
-	        # Dynamically load provider class
-			$class = "OAuth_".ucfirst($this->provider);
-			$this->oauth = new $class();
-		}
+        $class = "OAuth_".ucfirst($this->provider);
+        $this->oauth = new $class();
 
         # Install the application
         $this->oauth->install_app($this->app);
 
         # Configure the app's settings
-        $this->oauth->client_id       = $this->client_id;
-        $this->oauth->client_secret   = $this->client_secret;
-        $this->oauth->login_uri       = $this->login_uri;
+        $this->oauth->client_id       = $this->client_id; 
+        $this->oauth->client_secret   = $this->client_secret; 
+        $this->oauth->login_uri       = $this->login_uri; 
+        # Remove from settings from app now that they are stored in the oauth object
+        unset($this->app);
+        unset($this->provider);
+        unset($this->client_id);
+        unset($this->client_secret);
+        unset($this->login_uri);
     }
 
     public function get_user_session(){
         # First, do we already have a user session
         if( !$this->hasSession() ){
-	        # If not begin the oauth handshaking process
-	        $this->oauth->request_authorization_code();
-        } else {
-            return $_SESSION[$this->oauth->session_string]['access_token'];
+            # If not begin the oauth handshaking process
+            $this->oauth->request_authorization_code();
         }
     }
 
@@ -84,10 +87,17 @@ abstract class OAuthApp {
      * Activation description
      */
     public function activate(){
-    	if($this->url != '')
-    	header("Location: $this->url");
-		exit;
+        if($this->url != '')
+        header("Location: $this->url");
+        exit;
     }
+
+    /**
+     * Allow response to be parsed
+     *
+     * @param      array  $response
+     */
+    protected function parse($response){}
 }
 
 ?>
